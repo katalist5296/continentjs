2bfcc0a708d42d8bd9cd063664be4ef7b3f30fb2
katalist52 <katalist5296@gmail.com>
2021-01-03 11:51:56 +0300

    application context: initialization, startup, shutdown

--- packages/core/src/components/application.ts
+++ packages/core/src/components/application.ts
@@ -1,10 +1,19 @@
 export abstract class Application {
   private _initialized = false;
 
+  constructor() {
+    (async () => {
+      await this.onInitialize();
+
+      this._initialized = true;
+      this.onStarted();
+    })();
+  }
+
   get initialized(): boolean {
     return this._initialized;
   }
 
-  abstract onInitialize(): Promise<number>;
+  abstract onInitialize(): Promise<void>;
   abstract onStarted(): void;
 }
--- packages/server/src/core/server.ts
+++ packages/server/src/core/server.ts
@@ -1,5 +1,34 @@
 import { Application } from '@rage-framework/core';
+import { ShutdownSignal } from '../enums/shutdown-signal.enum';
 
 export abstract class Server extends Application {
-  abstract onShutdown(): Promise<void>;
+  constructor() {
+    super();
+
+    this.initShutdownHooks();
+  }
+
+  private initShutdownHooks() {
+    const signals = Object.keys(ShutdownSignal);
+    this.listenToShutdownSignals(signals);
+  }
+
+  private listenToShutdownSignals(signals: string[]) {
+    const cleanup = async (signal: string) => {
+      try {
+        signals.forEach((sig) => process.removeListener(sig, cleanup));
+        await this.onShutdown(signal);
+        process.kill(process.pid, signal);
+      } catch (err) {
+        console.log((err as Error)?.stack);
+        process.exit(1);
+      }
+    };
+
+    signals.forEach((signal: string) => {
+      process.on(signal as any, cleanup);
+    });
+  }
+
+  abstract onShutdown(signal: string): Promise<void>;
 }
